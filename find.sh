b960affdf132c0c5ef9bbabc51b7f909bfcf5db4
Mikgazer <carlolazzari4@gmail.com>
2021-02-15 22:20:38 +0100

    Update find.sh

--- find.sh
+++ find.sh
@@ -38,3 +38,50 @@ then
             fi
         fi
     done
+    
+    
+
+#!/bin.bash
+#controllo degli argomenti
+
+if test $# != 2
+   then
+      echo "Servono almeno  2 parametri"
+      exit 1
+fi
+
+dir=$1
+case "$dir" in
+    /*) ;;
+    *) echo " Errore: il secondo parametro deve essere una directory assoluta"  
+        exit 2;;
+esac
+
+file=$2
+if test ! -f $file  
+   then
+      echo "Il secondo parametro deve essere un file"
+      exit 3
+fi
+
+##############################################################################
+
+#RICERCA
+PATH=$PATH:`pwd`
+export PATH
+
+if test -x "$dir"
+then
+    for i in *
+    do
+      if test -d "$i"
+      then 
+          trova `pwd` "$i" "$file"
+      else
+          if test -f "$i" -a "$i"="$file"
+		  then
+              echo `pwd`/$i
+          fi
+       fi
+    done
+fi
